22a615934d507f4e740635ce38ffd2fe6fc90f40
Gabriel Cacabelos <gabi_cacabelos@hotmail.com>
2026-05-24 02:17:50 -0300

    fix general3

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from "next";
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval' https://assets.calendly.com https://us-assets.i.posthog.com https://prod.spline.design https://unpkg.com;
-  style-src 'self' 'unsafe-inline' https://assets.calendly.com https://fonts.googleapis.com;
-  font-src 'self' data: https://fonts.gstatic.com;
-  img-src 'self' data: blob: https:;
-  frame-src https://calendly.com;
-  connect-src 'self' blob: data: https: wss:;
-  media-src 'self' blob:;
-  worker-src 'self' blob: https:;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-`.replace(/\n/g, ' ').trim();
-
 const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
@@ -24,10 +9,6 @@ const nextConfig: NextConfig = {
       {
         source: '/:path*',
         headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: ContentSecurityPolicy,
-          },
           {
             key: 'X-DNS-Prefetch-Control',
             value: 'on'
